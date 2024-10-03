b25994fe93549228aac49d4e82a7bc68060365be
DevAndreww <andrea.sette2@marconibari.edu.it>
2024-10-03 15:28:36 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    float costo, risparmi, mancante, i, fgb;
+
+    cout << "Quanto costa la moto?" << endl;
+
+    cin >> costo;
+
+    cout << "Quanti sono i risparmi?" << endl;
+
+    cin >> risparmi;
+
+    i = 0;
+
+    while(costo>risparmi)
+    {
+        cout << "Quanti soldi inserirai oggi?" << endl;
+
+        cin >> fgb;
+
+        risparmi = risparmi+fgb;
+
+        mancante = costo-risparmi;
+
+        if(risparmi>costo)
+        {
+            cout << "Mancano " << mancante << "€." << endl;
+        }
+        else
+        {
+
+        }
+        i =i+1;
+    }
+    cout << "Sono stati effettuati " << i << " risparmi." << endl;
+
+    cout << "Sono stati accumulati " << risparmi << "€." << endl;
+
+    cout << "Sono rimasti " << -1*mancante << "€." << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
